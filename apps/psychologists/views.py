407328539dad8fd9d62b20c600b64ef7dbbde64a
francobenitez <franco.benitez@hogarthww.com>
2022-05-31 23:00:43 -0300

    feat: setup pagination for psychologists model

---
+++ apps/psychologists/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.core.paginator import Paginator 
+
+from .models import Psychologist
+
+def index(request):
+    psychologists = Psychologist.objects.all()
+
+    psychologist_paginator = Paginator(psychologists, 20)
+
+    page_num = request.GET.get('page')
+
+    page = psychologist_paginator.get_page(page_num)
+
+    context = {
+        'count' : psychologist_paginator.count,
+        'page' : page
+    }
+    return render(request, 'psychologists/index.html', context)
