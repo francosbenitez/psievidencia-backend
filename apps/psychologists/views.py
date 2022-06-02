6c821231ee60ce0b7fe1b7a29dc77958eeb95ff6
francobenitez <franco.benitez@hogarthww.com>
2022-06-02 01:21:50 -0300

    feat: implement pagination using DRF

--- apps/psychologists/views.py
+++ apps/psychologists/views.py
@@ -6,6 +6,7 @@ from .serializers import PsychologistSerializer
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
 
 def index(request):
   psychologists = Psychologist.objects.all()
@@ -22,6 +23,13 @@ class LatestPsychologistsList(APIView):
   def get(self, request, format=None):
     psychologists = Psychologist.objects.all()[0:4]
     serializer = PsychologistSerializer(psychologists, many=True)
-    print('serializer.data', serializer.data)
     return Response(serializer.data)
 
+class PaginatedPsychologists(APIView):
+  def get(self, request, format=None):
+    psychologists = Psychologist.objects.all()
+    paginator = PageNumberPagination()
+    paginator.page_size = 10
+    result_page = paginator.paginate_queryset(psychologists, request)
+    serializer = PsychologistSerializer(result_page, many=True)
+    return paginator.get_paginated_response(serializer.data)
