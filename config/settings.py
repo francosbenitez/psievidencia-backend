05a1ebe2eacc5c8501005080be7c18a3e129862a
francobenitez <franco.benitez@hogarthww.com>
2022-06-03 00:39:19 -0300

    feat: setup cors

--- config/settings.py
+++ config/settings.py
@@ -40,13 +40,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'apps.psychologists',
     'whitenoise.runserver_nostatic'
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
