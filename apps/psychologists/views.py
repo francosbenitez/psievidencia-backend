b6dd797d7694a9a71014306476107fa5a60f1b0c
francobenitez <franco.benitez@hogarthww.com>
2022-06-02 22:15:02 -0300

    feat: remove LatestPsychologists API & add PsychologistsDetail one

--- apps/psychologists/views.py
+++ apps/psychologists/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator 
+from django.http import Http404
 
 from .models import Psychologist
 from .serializers import PsychologistSerializer
@@ -17,8 +18,14 @@ class PaginatedPsychologists(APIView):
     serializer = PsychologistSerializer(result_page, many=True)
     return paginator.get_paginated_response(serializer.data)
 
-class LatestPsychologists(APIView):
-  def get(self, request, format=None):
-    psychologists = Psychologist.objects.all()[0:4]
-    serializer = PsychologistSerializer(psychologists, many=True)
+class PsychologistDetail(APIView):
+  def get_object(self, psychologist_id):
+    try:
+      return Psychologist.objects.get(id=psychologist_id)
+    except Psychologist.DoesNotExist:
+      raise Http404
+
+  def get(self, request, psychologist_id, format=None):
+    psychologist = self.get_object(psychologist_id)
+    serializer = PsychologistSerializer(psychologist)
     return Response(serializer.data)
