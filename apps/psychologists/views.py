b414cadbcd104d45f9e785f20e2f9daf1afc3ed7
francobenitez <franco.benitez@hogarthww.com>
2022-06-06 21:59:07 -0300

    feat: allow filtering by two fields more

--- apps/psychologists/views.py
+++ apps/psychologists/views.py
@@ -38,19 +38,33 @@ def search(request, format=None):
 
   name = None
   specialization = None
+  work_population = None
+  therapeutic_model = None
 
   if 'name' in request.GET:
     name = request.GET['name']
 
   if 'specialization' in request.GET:
     specialization = request.GET['specialization']
+
+  if 'work_population' in request.GET:
+    work_population = request.GET['work_population']
+
+  if 'therapeutic_model' in request.GET:
+    therapeutic_model = request.GET['therapeutic_model']
   
-  if name or specialization:
+  if name or specialization or work_population or therapeutic_model:
     if name is not None: 
         psychologists = psychologists.filter(name__icontains=name)
 
     if specialization is not None: 
         psychologists = psychologists.filter(specialization__icontains=specialization)
+
+    if work_population is not None: 
+        psychologists = psychologists.filter(work_population__icontains=work_population)
+
+    if therapeutic_model is not None: 
+        psychologists = psychologists.filter(therapeutic_model__icontains=therapeutic_model)
 
     serializer = PsychologistSerializer(psychologists, many=True)
     return Response(serializer.data)
