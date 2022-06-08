8c90f8c5e054e759d058a70c63da3ee6a276e992
francobenitez <franco.benitez@hogarthww.com>
2022-06-08 01:31:22 -0300

    chore: add notes

--- apps/psychologists/management/commands/processing.py
+++ apps/psychologists/management/commands/processing.py
@@ -20,11 +20,14 @@ class Command(BaseCommand):
             new_df["specialization"].value_counts().index.tolist(),
         )
         # TO-DO: 1) Make an API with the following structure:
-        # {
-        #   "specialization": "Primeros Auxilios Psicológicos",
-        #   "id": 1
-        # }
         # When people select the filter by specialization -let's say by "Sexología"-, it must appear the set of psychologists ("id") who have an specialization in "Sexología".
         # API: /api/psychologists?specialization[]=1
         # where "1" is the ID of the specialization.j
         # The result will be the set of psychologists who has that specialization.
+
+        # /api/psychologists?type=specialization
+        # This returns a JSON with all the specialization fields:
+        # {
+        #   "specialization": "Primeros Auxilios Psicológicos",
+        #   "id": 1
+        # }
