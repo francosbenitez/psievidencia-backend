d2ea8946067f92e67cee9dcd9620ab028ae77801
francobenitez <franco.benitez@hogarthww.com>
2022-07-15 00:06:59 -0300

    feat: add Suggestion model

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Suggestion(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=1000, default="")
+    description = models.CharField(max_length=1000, default="")
+    users = models.ManyToManyField(User, related_name="suggestions")
+
+    def __str__(self):
+        return self.name
