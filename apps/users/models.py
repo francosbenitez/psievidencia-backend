1d5aea0a9845c0db86a6ce69b1294ad84a07bd8b
francobenitez <franco.benitez@hogarthww.com>
2022-07-15 16:08:22 -0300

    fix: suggestion model

--- apps/users/models.py
+++ apps/users/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Suggestion(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=1000, default="")
     description = models.CharField(max_length=1000, default="")
     users = models.ManyToManyField(User, related_name="suggestions")
