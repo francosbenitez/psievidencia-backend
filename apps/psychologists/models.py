f8a248656a9a31794b6a0443ac929a1c18d138de
francobenitez <franco.benitez@hogarthww.com>
2022-07-26 01:03:21 -0300

    chore: update todo

--- apps/psychologists/models.py
+++ apps/psychologists/models.py
@@ -29,6 +29,7 @@ class Psychologist(models.Model):
     phone_number = models.CharField(max_length=1000, default="")
     additional_data = models.CharField(max_length=1000, default="")
     name_2 = models.CharField(max_length=1000, default="")
+    liked = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
