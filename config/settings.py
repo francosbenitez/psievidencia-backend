ff8bd9b510b26de2fe8932da2d77bea4c0fc4911
francobenitez <franco.benitez@hogarthww.com>
2022-08-23 18:38:01 -0300

    feat: implement env(secret_key)

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-e=zhu9d_dl2aik^hb)f75pcipfe29^#x!m2ekf@%&)=y5!%68d"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
