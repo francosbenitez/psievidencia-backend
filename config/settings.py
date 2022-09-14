109627124d1894ca5be08107360ba092872e12e7
francobenitez <franco.benitez@hogarthww.com>
2022-09-14 09:28:09 -0300

    refactor: modify template & settings to allow custom email domain

--- config/settings.py
+++ config/settings.py
@@ -163,6 +163,7 @@ MEDIA_URL = "/media/"
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = env("EMAIL_HOST")
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_PORT = 587
 EMAIL_HOST_USER = env("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
