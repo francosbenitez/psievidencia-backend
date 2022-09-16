199d26a567d20442594eab8b31181b672c368a73
francobenitez <franco.benitez@hogarthww.com>
2022-09-15 22:25:03 -0300

    feat: add serializers snippets for resetting password

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -34,7 +34,46 @@ class SuggestionSerializer(serializers.ModelSerializer):
         fields = ("id", "title", "description")
 
 
-class FavoriteSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Favorite
-        fields = ("id", "psychologist_id", "user_id")
+# class FavoriteSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Favorite
+#         fields = ("id", "psychologist_id", "user_id")
+
+# class ResetPasswordEmailRequestSerializer(serializers.Serializer):
+#     email = serializers.EmailField(min_length=2)
+
+#     redirect_url = serializers.CharField(max_length=500, required=False)
+
+#     class Meta:
+#         fields = ['email']
+
+
+# class SetNewPasswordSerializer(serializers.Serializer):
+#     password = serializers.CharField(
+#         min_length=6, max_length=68, write_only=True)
+#     token = serializers.CharField(
+#         min_length=1, write_only=True)
+#     uidb64 = serializers.CharField(
+#         min_length=1, write_only=True)
+
+#     class Meta:
+#         fields = ['password', 'token', 'uidb64']
+
+#     def validate(self, attrs):
+#         try:
+#             password = attrs.get('password')
+#             token = attrs.get('token')
+#             uidb64 = attrs.get('uidb64')
+
+#             id = force_str(urlsafe_base64_decode(uidb64))
+#             user = User.objects.get(id=id)
+#             if not PasswordResetTokenGenerator().check_token(user, token):
+#                 raise AuthenticationFailed('The reset link is invalid', 401)
+
+#             user.set_password(password)
+#             user.save()
+
+#             return (user)
+#         except Exception as e:
+#             raise AuthenticationFailed('The reset link is invalid', 401)
+#         return super().validate(attrs)
