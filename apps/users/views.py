307afb66f3d0e1f60592895d9b748ed223de4bff
francobenitez <franco.benitez@hogarthww.com>
2022-09-15 22:21:18 -0300

    feat: add request password snippets

--- apps/users/views.py
+++ apps/users/views.py
@@ -237,3 +237,38 @@ class FavoritesList(APIView):
 
         serializer = PsychologistSerializer(favorites_psychologists, many=True)
         return Response(serializer.data)
+
+
+# class RequestPasswordResetEmail(generics.GenericAPIView):
+#     serializer_class = ResetPasswordEmailRequestSerializer
+
+#     def post(self, request):
+#         serializer = self.serializer_class(data=request.data)
+
+#         email = request.data.get('email', '')
+
+#         if User.objects.filter(email=email).exists():
+#             user = User.objects.get(email=email)
+#             uidb64 = urlsafe_base64_encode(smart_bytes(user.id))
+#             token = PasswordResetTokenGenerator().make_token(user)
+#             current_site = get_current_site(
+#                 request=request).domain
+#             relativeLink = reverse(
+#                 'password-reset-confirm', kwargs={'uidb64': uidb64, 'token': token})
+
+#             redirect_url = request.data.get('redirect_url', '')
+#             absurl = 'http://'+current_site + relativeLink
+#             email_body = 'Hello, \n Use link below to reset your password  \n' + \
+#                 absurl+"?redirect_url="+redirect_url
+#             data = {'email_body': email_body, 'to_email': user.email,
+#                     'email_subject': 'Reset your passsword'}
+#             Util.send_email(data)
+#         return Response({'success': 'We have sent you a link to reset your password'}, status=status.HTTP_200_OK)
+
+# class SetNewPasswordAPIView(generics.GenericAPIView):
+#     serializer_class = SetNewPasswordSerializer
+
+#     def patch(self, request):
+#         serializer = self.serializer_class(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         return Response({'success': True, 'message': 'Password reset success'}, status=status.HTTP_200_OK)
