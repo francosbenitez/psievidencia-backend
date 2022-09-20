f17809e2ebec4c56567a3653ae5d687b5d80ff0b
francobenitez <franco.benitez@hogarthww.com>
2022-09-20 19:29:30 -0300

    chore(cleaning up): remove suggestions app

--- apps/suggestions/serializers.py
+++
@@ -1,7 +0,0 @@
-from .models import Suggestion
-
-
-class SuggestionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Suggestion
-        fields = ("id", "title", "description")
--- apps/suggestions/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from apps.suggestions import views
-
-urlpatterns = [
-    path("suggestions", views.SuggestionsList.as_view()),
-    path("suggestions/create", views.CreateSuggestion.as_view()),
-]
