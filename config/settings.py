f51ece56b9b9adbb9432c2c78203f4c365f50274
Franco <francosbenitez@gmail.com>
2022-11-12 01:18:56 -0300

    fix: add csrf_truted_origins v2

--- config/settings.py
+++ config/settings.py
@@ -35,7 +35,10 @@ ALLOWED_HOSTS = [
     "*",
 ]
 
-CSRF_TRUSTED_ORIGINS = ["*"]
+CSRF_TRUSTED_ORIGINS = [
+    "https://web-develop-ecb4.up.railway.app",
+    "https://web-main.up.railway.app",
+]
 
 # Application definition
 
