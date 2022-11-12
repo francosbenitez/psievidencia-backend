80dab1867f227e61e7ad577999be05ac940c3fe6
Franco <francosbenitez@gmail.com>
2022-11-12 01:22:58 -0300

    feat: register remaining models

--- apps/psychologists/admin.py
+++ apps/psychologists/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from .models import (
+    Psychologist,
     Specialization,
     TherapeuticModel,
     WorkPopulation,
@@ -9,13 +10,16 @@ from .models import (
     Province,
     GenderIdentity,
     GenderPerspective,
+    Prepaid,
 )
 
+admin.site.register(Psychologist)
 admin.site.register(Specialization)
 admin.site.register(TherapeuticModel)
 admin.site.register(WorkPopulation)
 admin.site.register(WorkModality)
 admin.site.register(Province)
 admin.site.register(Education)
-admin.site.register(GenderIdentity)
 admin.site.register(GenderPerspective)
+admin.site.register(Prepaid)
+admin.site.register(GenderIdentity)
