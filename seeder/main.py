408cb57a07cd8eac91e7be98167b3aadfb0c6752
Franco <francosbenitez@gmail.com>
2022-11-16 00:01:25 -0300

    feat: add seeder/main v4

--- seeder/main.py
+++ seeder/main.py
@@ -5,13 +5,20 @@ import unidecode
 from datetime import datetime, date
 from utils import update_csv
 import secrets
+from setup import setup_django
 
 # from django.template.loader import render_to_string
 # from django.core.mail import send_mail
 # from django.conf import settings
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-django.setup()
+
+PROJECT_PATH = "../"
+PROJECT_SETTING = "config.settings"
+
+setup_django(PROJECT_PATH, PROJECT_SETTING)
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+# django.setup()
 
 import csv
 from apps.psychologists.models import (
---
+++ seeder/setup.py
@@ -0,0 +1,12 @@
+def setup_django(root_path, settings):
+    import os
+    import django
+    import sys
+
+    os.chdir(root_path)
+
+    # Django settings
+    sys.path.append(root_path)
+    os.environ["DJANGO_SETTINGS_MODULE"] = settings
+
+    django.setup()
