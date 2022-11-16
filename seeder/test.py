539eaeb2e95629a6b540dd9c1a2a1f779a8d620a
Franco <francosbenitez@gmail.com>
2022-11-15 23:02:45 -0300

    refactor: remove PYTHONPATH

--- seeder/test.py
+++ seeder/test.py
@@ -1,1 +1,42 @@
-print("hello world")
+import os
+import django
+import pandas as pd
+import unidecode
+from datetime import datetime, date
+from utils import update_csv
+import secrets
+
+import os.path, sys
+
+sys.path.insert(0, os.path.dirname(os.path.dirname("./")))
+
+# from django.template.loader import render_to_string
+# from django.core.mail import send_mail
+# from django.conf import settings
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+django.setup()
+
+import csv
+from apps.psychologists.models import (
+    Psychologist,
+    Specialization,
+    TherapeuticModel,
+    WorkPopulation,
+    Education,
+    GenderPerspective,
+    Prepaid,
+    GenderIdentity,
+    WorkModality,
+    Province,
+)
+
+from apps.users.models import Authenticated
+
+
+def main_seeder():
+    print("hello world")
+
+
+if __name__ == "__main__":
+    main_seeder()
