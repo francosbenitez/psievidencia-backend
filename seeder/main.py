96de3d168be8249507888d8891d026e39b0aed9c
Franco <francosbenitez@gmail.com>
2022-11-16 00:34:42 -0300

    refactor; remove test-workflow & modify update-script based on the tests carried out

--- seeder/main.py
+++ seeder/main.py
@@ -7,18 +7,10 @@ from utils import update_csv
 import secrets
 from setup import setup_django
 
-# from django.template.loader import render_to_string
-# from django.core.mail import send_mail
-# from django.conf import settings
-
-
 PROJECT_PATH = "./"
 PROJECT_SETTING = "config.settings"
 
 setup_django(PROJECT_PATH, PROJECT_SETTING)
-
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-# django.setup()
 
 import csv
 from apps.psychologists.models import (
--- seeder/setup.py
+++ seeder/setup.py
@@ -4,14 +4,6 @@ def setup_django(root_path, settings):
     import sys
 
     os.chdir(root_path)
-
-    print("os.chdir", os.chdir)
-
-    # Django settings
     sys.path.append(root_path)
-
-    print("sys.path", sys.path)
-
     os.environ["DJANGO_SETTINGS_MODULE"] = settings
-
     django.setup()
