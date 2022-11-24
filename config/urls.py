a990650ba5cddd98d94198bffdadb9e9d40766b0
Franco <francosbenitez@gmail.com>
2022-11-23 23:46:57 -0300

    feat: implement drf_yasg

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,27 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Psievidencia API",
+        default_version="v1",
+        description="The API for psievidencia.com",
+        contact=openapi.Contact(email="hola@psievidencia.com"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path(
+        "",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
