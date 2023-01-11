808b23fa65598446fe7fcd29fb09698a179e8248
Franco <francosbenitez@gmail.com>
2023-01-10 23:28:41 -0300

    refactor: allowed_hosts

--- config/settings.py
+++ config/settings.py
@@ -31,9 +31,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "*",
-]
+ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(' ')
 
 CSRF_TRUSTED_ORIGINS = [
     "https://web-develop-ecb4.up.railway.app",
