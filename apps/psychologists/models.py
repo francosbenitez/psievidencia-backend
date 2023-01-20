77a265d63c7e5873613149005824db6b8cade6c8
Franco <francosbenitez@gmail.com>
2023-01-19 23:08:25 -0300

    feat: improve psychologists model

--- apps/psychologists/models.py
+++ apps/psychologists/models.py
@@ -3,34 +3,41 @@ import apps.psychologists.constants as constants
 from apps.accounts.models import User
 
 
-class SocialNetwork(models.Model):
-    facebook = models.CharField(max_length=255, blank=True, null=True)
-    twitter = models.CharField(max_length=255, blank=True, null=True)
-    instagram = models.CharField(max_length=255, blank=True, null=True)
-    whatsapp = models.CharField(max_length=255, blank=True, null=True)
-
-
-class Prepaid(models.Model):
-    has = models.BooleanField(default=False)
-    name = models.CharField(max_length=255, blank=True)
-
-
-class SocialWork(models.Model):
-    has = models.BooleanField(default=False)
-    name = models.CharField(max_length=255, blank=True)
-
-
 class Institution(models.Model):
-    has = models.BooleanField(default=False)
     name = models.CharField(max_length=255, blank=True)
     url = models.CharField(max_length=255, blank=True)
 
 
-class Registration(models.Model):
+class RegularHealth(models.Model):
+    name = models.CharField(max_length=255, blank=True)
+
+
+class PrepaidHealth(models.Model):
+    name = models.CharField(max_length=255, blank=True)
+
+
+class Language(models.Model):
     name = models.CharField(
-        choices=constants.REGISTRATION_NAMES, max_length=255, blank=False, null=False
+        max_length=10,
+        choices=constants.LANGUAGES,
+        null=True,
     )
-    number = models.IntegerField()
+
+
+class Specialization(models.Model):
+    name = models.CharField(max_length=255, null=True)
+
+
+class TherapeuticModel(models.Model):
+    name = models.CharField(max_length=255, null=True)
+
+
+class WorkPopulation(models.Model):
+    name = models.CharField(max_length=255, null=True)
+
+
+class WorkModality(models.Model):
+    name = models.CharField(max_length=255, null=True)
 
 
 class Psychologist(User):
@@ -41,49 +48,30 @@ class Psychologist(User):
     education = models.CharField(
         choices=constants.EDUCATIONS, max_length=255, null=False, blank=False
     )
-    social_networks = models.ForeignKey(
-        SocialNetwork, on_delete=models.CASCADE, null=True
+    instagram_profile = models.CharField(max_length=255, blank=True, null=True)
+    linkedin_profile = models.CharField(max_length=255, blank=True, null=True)
+    registration_name = models.CharField(
+        choices=constants.REGISTRATION_NAMES, max_length=255, blank=False, null=False
     )
-    institution = models.ForeignKey(Institution, on_delete=models.CASCADE, null=True)
-    prepaid = models.ForeignKey(Prepaid, on_delete=models.CASCADE, null=True)
-    social_work = models.ForeignKey(SocialWork, on_delete=models.CASCADE, null=True)
-    registration = models.ForeignKey(Registration, on_delete=models.CASCADE, null=True)
-    has_perspective = models.BooleanField(default=False)
-    has_team = models.BooleanField(default=False)
+    registration_number = models.PositiveIntegerField(null=False)
+    has_gender_perspective = models.BooleanField(default=False)
+    has_work_team = models.BooleanField(default=False)
     offers_invoice = models.BooleanField(default=False)
     offers_online = models.BooleanField(default=False)
-    offers_first_free = models.BooleanField(default=False)
+    offers_first_session_free = models.BooleanField(default=False)
     knows_sign_language = models.BooleanField(default=False)
-    price_hour = models.DecimalField(
-        decimal_places=2, max_digits=20, blank=True, null=True
-    )
-    liked = models.BooleanField(default=False)
+    price_hour = models.DecimalField(decimal_places=2, max_digits=20, null=True)
 
+    rating_average = models.FloatField(default=0)
+    review_count = models.IntegerField(default=0)
 
-class Language(models.Model):
-    name = models.CharField(
-        max_length=10,
-        choices=constants.LANGUAGES,
-        null=True,
-    )
-    psychologists = models.ManyToManyField(Psychologist)
+    institution = models.ManyToManyField(Institution)
+    regular_health = models.ManyToManyField(RegularHealth)
+    prepaid_health = models.ManyToManyField(RegularHealth)
+    language = models.ManyToManyField(Language)
+    specialization = models.ManyToManyField(Specialization)
+    therapeutic_model = models.ManyToManyField(TherapeuticModel)
+    work_population = models.ManyToManyField(WorkPopulation)
+    work_modality = models.ManyToManyField(WorkModality)
 
-
-class Specialization(models.Model):
-    name = models.CharField(max_length=255, null=True)
-    psychologists = models.ManyToManyField(Psychologist)
-
-
-class TherapeuticModel(models.Model):
-    name = models.CharField(max_length=255, null=True)
-    psychologists = models.ManyToManyField(Psychologist)
-
-
-class WorkPopulation(models.Model):
-    name = models.CharField(max_length=255, null=True)
-    psychologists = models.ManyToManyField(Psychologist, blank=True)
-
-
-class WorkModality(models.Model):
-    name = models.CharField(max_length=255, null=True)
-    psychologists = models.ManyToManyField(Psychologist)
+    was_liked = models.BooleanField(default=False)
